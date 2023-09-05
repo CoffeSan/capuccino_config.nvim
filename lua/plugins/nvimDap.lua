f3db70d9ce67cd333e808e5da87b358c4bc6e27a
Fabricio Duarte <fabricioohod@outlook.com>
2023-09-05 17:30:20 -0300

    re-write config

---
+++ lua/plugins/nvimDap.lua
@@ -0,0 +1,20 @@
+return {
+	"mfussenegger/nvim-dap",
+	config = function ()
+		local dap = require("dap")
+
+		dap.adapters.unity = {
+			type = 'executable',
+			command = '<path-to-mono-directory>/Commands/mono',
+			args = {'<path-to-unity-debug-directory>/unity.unity-debug-x.x.x/bin/UnityDebug.exe'}
+		}
+		dap.configurations.cs = {
+			{
+				type = 'unity',
+				request = 'attach',
+				name = 'Unity Editor',
+			}
+		}
+
+	end
+}
