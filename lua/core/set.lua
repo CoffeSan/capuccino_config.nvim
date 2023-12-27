d95693c34c1e104bf61ea5ff9ffa1b914f99ff22
Fabricio Duarte <fabricioohod@outlook.com>
2023-12-27 11:54:29 -0300

    Update set

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -1,16 +1,27 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.o.hlsearch = false
-vim.opt.number = true
 vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes'
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.colorcolumn = "90"
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.o.hlsearch = false
 vim.o.mouse = 'a'
 vim.o.clipboard = 'unnamedplus'
 vim.o.breakindent = true
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.wo.signcolumn = 'yes'
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
