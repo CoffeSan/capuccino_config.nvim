5f7d9eb8d63639e9351e2b0513a1d63f165e778c
Fabricio Duarte <fabricioohod@outlook.com>
2024-01-12 15:30:25 -0300

    Batter comments in keyBinds.lua

--- lua/core/keyBinds.lua
+++ lua/core/keyBinds.lua
@@ -1,109 +1,85 @@
+-- Define local variables for keymap and command functions
 local keymap = vim.keymap
 local cmd = vim.cmd
 
--- Neo Vim Binds
-keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+--- Neo Vim Binds
+keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })  -- Map Space to No operation (Nop) in normal and visual mode
+keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })  -- Map k to gk or k based on count in normal mode
+keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })  -- Map j to gj or j based on count in normal mode
 
--- Save open buffer
-keymap.set('n', '<C-s>', cmd.wall, { desc = 'Save Current File' })
--- Close current open buffer
-keymap.set('n', '<C-Q>', cmd.bd)
+--- Save and Close Binds
+keymap.set('n', '<C-s>', cmd.wall, { desc = 'Save Current File' })  -- Map Ctrl + S to save the current file
+keymap.set('n', '<C-Q>', cmd.bd)  -- Map Ctrl + Q to close the current buffer
 
--- Go to next open buffer
-keymap.set('n', '<C-N>', ':bnext<CR>')
--- Go to previous open buffer
-keymap.set('n', '<C-P>', ':bprev<CR>')
+--- Buffer Navigation Binds
+keymap.set('n', '<C-N>', ':bnext<CR>')  -- Map Ctrl + N to go to the next open buffer
+keymap.set('n', '<C-P>', ':bprev<CR>')  -- Map Ctrl + P to go to the previous open buffer
 
-keymap.set('t', '<C-X>', "<C-\\><C-n><C-w>h")
+--- Terminal Mode Bind
+keymap.set('t', '<C-X>', "<C-\\><C-n><C-w>h")  -- Map Ctrl + X to move cursor to the split window on the left
 
--- Move line up
-keymap.set('n', '<A-j>', ':m +1<CR>')
--- Move line down
-keymap.set('n', '<A-k>', ':m -2<CR>')
+--- Move Line Binds
+keymap.set('n', '<A-j>', ':m +1<CR>')  -- Map Alt + j to move the current line down
+keymap.set('n', '<A-k>', ':m -2<CR>')  -- Map Alt + k to move the current line up
 
--- Harpoon Binds
+--- Harpoon Binds
 local harpoon = require("harpoon")
-keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = 'Append to buffers list' })
-keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Show buffers list' })
-keymap.set("n", "<A-1>", function() harpoon:list():select(1) end, { desc = 'Go to first buffer' })
-keymap.set("n", "<A-2>", function() harpoon:list():select(2) end, { desc = 'Go to seccond buffer' })
-keymap.set("n", "<A-3>", function() harpoon:list():select(3) end, { desc = 'Go to third buffer' })
-keymap.set("n", "<A-4>", function() harpoon:list():select(4) end, { desc = 'Go to fourth buffer' })
+keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = 'Append to buffers list' })  -- Map leader + a to append buffer to the Harpoon list
+keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Show buffers list' })  -- Map Ctrl + E to toggle quick menu for Harpoon list
+keymap.set("n", "<A-1>", function() harpoon:list():select(1) end, { desc = 'Go to first buffer' })  -- Map Alt + 1 to go to the first buffer in the Harpoon list
+keymap.set("n", "<A-2>", function() harpoon:list():select(2) end, { desc = 'Go to second buffer' })  -- Map Alt + 2 to go to the second buffer in the Harpoon list
+keymap.set("n", "<A-3>", function() harpoon:list():select(3) end, { desc = 'Go to third buffer' })  -- Map Alt + 3 to go to the third buffer in the Harpoon list
+keymap.set("n", "<A-4>", function() harpoon:list():select(4) end, { desc = 'Go to fourth buffer' })  -- Map Alt + 4 to go to the fourth buffer in the Harpoon list
 
--- Toggle previous & next buffers stored within Harpoon list
-keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = 'Previous buffer' })
-keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = 'Next buffer' })
+--- Toggle Buffers Binds
+keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = 'Previous buffer' })  -- Map Ctrl + Shift + P to go to the previous buffer in the Harpoon list
+keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = 'Next buffer' })  -- Map Ctrl + Shift + N to go to the next buffer in the Harpoon list
 
--- cmp Binds
+--- cmp Binds
 local cmp = require("cmp")
 cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-    ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<C-k>"] = cmp.mapping.select_prev_item(),  -- Map Ctrl + K to select the previous suggestion
+    ["<C-j>"] = cmp.mapping.select_next_item(),  -- Map Ctrl + J to select the next suggestion
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),      -- Map Ctrl + B to scroll documentation up
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),       -- Map Ctrl + F to scroll documentation down
+    ["<C-Space>"] = cmp.mapping.complete(),       -- Map Ctrl + Space to show completion suggestions
+    ["<C-e>"] = cmp.mapping.abort(),              -- Map Ctrl + E to close the completion window
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),  -- Map Enter to confirm without selecting
 })
 
+--- oil Bind
+keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })  -- Map - to open the parent directory using Oil
 
--- oil Bind
-keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
--- Telescope Binds
+--- Telescope Binds
 local telescope = require('telescope.builtin')
-keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Find File' })
-keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Find With Grep' })
-keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Find Open Buffer' })
+keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Find File' })  -- Map leader + ff to find files using Telescope
+keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Find With Grep' })  -- Map leader + fg to live grep using Telescope
+keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Find Open Buffer' })  -- Map leader + fb to find open buffers using Telescope
 
--- Lsp config Binds
+--- Lsp config Binds
 local lspBuf = vim.lsp.buf
 local diagnostics = vim.diagnostic
 local opts = { noremap = true, silent = true }
 
+--- LSP References and Formatting Binds
 opts.desc = "Show LSP references"
-keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)  -- Map gR to show LSP references using Telescope
 
 opts.desc = "Format current file"
-keymap.set("n", "F", lspBuf.format, opts)
+keymap.set("n", "F", lspBuf.format, opts)  -- Map F to format the current file using LSP
 
+--- LSP Navigation Binds
 opts.desc = "Go to declaration"
-keymap.set("n", "gD", lspBuf.declaration, opts)
+keymap.set("n", "gD", lspBuf.declaration, opts)  -- Map gD to go to declaration using LSP
 
 opts.desc = "Show LSP definitions"
-keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)  -- Map gd to show LSP definitions using Telescope
 
 opts.desc = "Show LSP implementations"
-keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)  -- Map gi to show LSP implementations using Telescope
 
 opts.desc = "Show LSP type definitions"
-keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)  -- Map gt to show LSP type definitions using Telescope
 
-opts.desc = "See available code actions"
-keymap.set({ "n", "v" }, "<leader>ca", lspBuf.code_action, opts)
-
-opts.desc = "Smart rename"
-keymap.set("n", "<leader>rn", lspBuf.rename, opts)
-
-opts.desc = "Show buffer diagnostics"
-keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-
-opts.desc = "Show line diagnostics"
-keymap.set("n", "<leader>d", diagnostics.open_float, opts)
-
-opts.desc = "Go to previous diagnostic"
-keymap.set("n", "[d", diagnostics.goto_prev, opts)
-
-opts.desc = "Go to next diagnostic"
-keymap.set("n", "]d", diagnostics.goto_next, opts)
-
-opts.desc = "Show documentation for what is under cursor"
-keymap.set("n", "K", lspBuf.hover, opts)
-
-opts.desc = "Restart LSP"
-keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
-
-opts.desc = "Format current buffer"
-keymap.set("n", "<C-f>", lspBuf.format, opts)
+--- LSP Code Actions Binds
+opts.desc = "See available code"
