5bfe93c36f12476b00e2981b36ff2c5a1ddc71b4
Fabricio Duarte <fabricioohod@outlook.com>
2024-02-24 14:16:11 -0300

    feat: Disable transparent bg on Neovim Qt

--- lua/plugins/list/colorScheme.lua
+++ lua/plugins/list/colorScheme.lua
@@ -1,45 +1,49 @@
 return {
-	{
-		"catppuccin/nvim",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
-				background = { -- :h background
-					light = "macchiato",
-					dark = "mocha",
-				},
-				transparent_background = false, -- disables setting the background color.
-				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-				dim_inactive = {
-					enabled = false, -- dims the background color of inactive window
-					shade = "dark",
-					percentage = 0.15, -- percentage of the shade to apply to the inactive window
-				},
-				no_italic = false,  -- Force no italic
-				no_bold = false,    -- Force no bold
-				no_underline = false, -- Force no underline
-				styles = {          -- Handles the styles of general hi groups (see `:h highlight-args`):
-					comments = { "italic" }, -- Change the style of comments
-					conditionals = { "italic" },
-				},
-				color_overrides = {},
-				custom_highlights = {},
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					treesitter = true,
-					telescope = {
-						enabled = true,
-					},
-					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-				},
-			})
+    {
+        "catppuccin/nvim",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                background = {     -- :h background
+                    light = "macchiato",
+                    dark = "mocha",
+                },
+                transparent_background = false, -- disables setting the background color.
+                show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+                term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+                dim_inactive = {
+                    enabled = false,            -- dims the background color of inactive window
+                    shade = "dark",
+                    percentage = 0.15,          -- percentage of the shade to apply to the inactive window
+                },
+                no_italic = false,              -- Force no italic
+                no_bold = false,                -- Force no bold
+                no_underline = false,           -- Force no underline
+                styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+                    comments = { "italic" },    -- Change the style of comments
+                    conditionals = { "italic" },
+                },
+                color_overrides = {},
+                custom_highlights = {},
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    treesitter = true,
+                    telescope = {
+                        enabled = true,
+                    },
+                    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+                },
+            })
 
-			vim.cmd.colorscheme "catppuccin-macchiato"
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		end
-	}
+            vim.cmd.colorscheme "catppuccin-macchiato"
+
+            if vim.env.NVIM_QT_GUI ~= nil then
+                vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            end
+
+        end
+    }
 }
