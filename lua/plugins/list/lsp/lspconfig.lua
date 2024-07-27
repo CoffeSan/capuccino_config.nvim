591951e6e064049daf018f881a1c4feef337c814
Fabricio.Duarte <_>
2024-07-26 22:30:40 -0300

    Fix: some importing erros

--- lua/plugins/list/lsp/lspconfig.lua
+++ lua/plugins/list/lsp/lspconfig.lua
@@ -1,15 +1,15 @@
 return {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    event = {"BufReadPre", "BufNewFile"},
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
-        { "antosha417/nvim-lsp-file-operations", config = true },
+        {"antosha417/nvim-lsp-file-operations", config = true}
     },
     config = function()
         local lspconfig = require("lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        local opts = { noremap = true, silent = true }
+        local opts = {noremap = true, silent = true}
         local on_attach = function(_, bufnr)
             opts.buffer = bufnr
         end
@@ -19,40 +19,55 @@ return {
 
         -- Change the Diagnostic symbols in the sign column (gutter)
         -- (not in youtube nvim video)
-        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+        local signs = {Error = " ", Warn = " ", Hint = "󰠠 ", Info = " "}
         for type, icon in pairs(signs) do
             local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+            vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
         end
 
-        local servers = require("plugins.list.lsp.servers")
+        local servers = {
+            "csharp_ls",
+            "omnisharp",
+            "rust_analyzer",
+            "tsserver",
+            "html",
+            "tailwindcss",
+            "svelte",
+            "lua_ls",
+            "gopls"
+        }
 
         for _, value in pairs(servers) do
-            lspconfig[value].setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
+            lspconfig[value].setup(
+                {
+                    capabilities = capabilities,
+                    on_attach = on_attach
+                }
+            )
         end
 
         -- configure lua server (with special settings)
-        lspconfig["lua_ls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = { -- custom settings for lua
-                Lua = {
-                    -- make the language server recognize "vim" global
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                    workspace = {
-                        -- make language server aware of runtime files
-                        library = {
-                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                            [vim.fn.stdpath("config") .. "/lua"] = true,
+        lspconfig["lua_ls"].setup(
+            {
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    -- custom settings for lua
+                    Lua = {
+                        -- make the language server recognize "vim" global
+                        diagnostics = {
+                            globals = {"vim"}
                         },
-                    },
-                },
-            },
-        })
-    end,
+                        workspace = {
+                            -- make language server aware of runtime files
+                            library = {
+                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                [vim.fn.stdpath("config") .. "/lua"] = true
+                            }
+                        }
+                    }
+                }
+            }
+        )
+    end
 }
--- lua/plugins/list/lsp/mason.lua
+++ lua/plugins/list/lsp/mason.lua
@@ -4,6 +4,17 @@ return {
     config = function()
         local mason = require("mason")
         local mason_lspconfig = require("mason-lspconfig")
+        local servers = {
+            "csharp_ls",
+            "omnisharp",
+            "rust_analyzer",
+            "tsserver",
+            "html",
+            "tailwindcss",
+            "svelte",
+            "lua_ls",
+            "gopls"
+        }
 
         mason.setup({
             ui = {
@@ -15,7 +26,7 @@ return {
             }
         })
         mason_lspconfig.setup({
-            ensure_installed = require("plugins.list.lsp.servers"),
+            ensure_installed = servers,
             automatic_installation = true
         })
     end
